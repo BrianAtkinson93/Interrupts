354c5039527c3b9e66fa84cc06b13929e66ecb0c
Brian Atkinson <BrianAtkinson72@gmail.com>
2024-05-09 17:24:04 -0700

    Added a dictionary to allow for output of the signal descriptions

--- src/main.c
+++ src/main.c
@@ -1,14 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
+#include <stdio.h>  // Input and output functions
+#include <stdlib.h> // Standard functions like exit
+#include <signal.h> // For signal handling and macros
+#include <unistd.h> // Used for sleep
+
+// Structure to hold the table information
+typedef struct
+{
+    int sig_num;
+    const char *sig_name;
+    const char *action;
+    const char *description;
+} signal_info;
+
+// Dictionary of signals and descriptions.
+signal_info signals[] = {
+    {SIGHUP, "SIGHUP", "Terminate", "Hangup"},
+    {SIGINT, "SIGINT", "Terminate", "Terminal interrupt signal"},
+    {SIGQUIT, "SIGQUIT", "Terminate (core dump)", "Terminal quit signal"},
+    {SIGILL, "SIGILL", "Terminate (core dump)", "Illegal instruction"},
+    {SIGTRAP, "SIGTRAP", "Terminate (core dump)", "Trace/breakpoint trap"},
+    {SIGABRT, "SIGABRT", "Terminate (core dump)", "Process abort signal"},
+    {SIGFPE, "SIGFPE", "Terminate (core dump)", "Erroneous arithmetic operation"},
+    {SIGKILL, "SIGKILL", "Terminate", "Kill (cannot be caught or ignored)"},
+    {SIGSEGV, "SIGSEGV", "Terminate (core dump)", "Invalid memory reference"},
+    {SIGPIPE, "SIGPIPE", "Terminate", "Write on a pipe with no one to read it"},
+    {SIGALRM, "SIGALRM", "Terminate", "Alarm clock"},
+    {SIGTERM, "SIGTERM", "Terminate", "Termination signal"},
+    {0, NULL, NULL, NULL} // Terminator
+};
 
 // Signal handler function
 void handle_interrupt(int sig)
 {
-    printf("Interrupt received: SIGALRM\n");
-    // Signal handling code here
-    // After handling, you might reset the timer or handle the data
+    // Search for the signal in the array
+    for (int i = 0; signals[i].sig_num != 0; i++)
+    {
+        if (signals[i].sig_num == sig)
+        {
+            printf("Interrupt received: %s\n", signals[i].sig_name);
+            printf("Description: %s\n", signals[i].description);
+            printf("Action: %s\n", signals[i].action);
+            break;
+        }
+    }
+    // printf("Interrupt received: SIGALRM\n");
+    // // Signal handling code here
+    // // After handling, you might reset the timer or handle the data
 
     // Exit program or do additional tasks here
     printf("Handling complete, program will exit now.\n");
